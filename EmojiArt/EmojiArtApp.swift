9c59603b26a4b5074cbef68a921e7b47a26c2ac6
piyush-imad <piyush9401@gmail.com>
2023-04-18 12:16:55 +0530

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Piyush Sharma on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
